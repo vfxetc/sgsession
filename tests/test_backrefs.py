a6840749fee98956dae3962abbdc396e85b79ba5
Mike Boers <mboers@keypics.com>
2012-09-11 16:25:31 -0700

    test_backrefs uses sgmock

--- tests/test_backrefs.py
+++ tests/test_backrefs.py
@@ -1,16 +1,27 @@
-from .utils import *
-
-
-def setUpModule():
-    fixtures.setup_tasks()
-    globals().update(fixtures.__dict__)
-    
+from common import *
     
 class TestBackrefs(TestCase):
-
+    
+    def setUp(self):
+        sg = Shotgun()
+        self.sg = self.fix = fix = Fixture(sg)
+        self.session = Session(self.sg)
+        
+        proj = fix.Project(mini_uuid())
+        seqs = [proj.Sequence(code, project=proj) for code in ('AA', 'BB')]
+        shots = [seq.Shot('%s_%03d' % (seq['code'], i), project=proj) for seq in seqs for i in range(1, 3)]
+        steps = [fix.find_or_create('Step', name=name, short_name=name) for name in ('Anm', 'Comp', 'Model')]
+        tasks = [shot.Task(step['name'] + ' something', step=step, entity=shot, project=proj) for step in steps for shot in shots]
+        
+        self.proj = minimal(proj)
+        self.seqs = [minimal(x) for x in seqs]
+        self.shots = [minimal(x) for x in shots]
+        self.steps = [minimal(x) for x in steps]
+        self.tasks = [minimal(x) for x in tasks]
+    
     def test_backref(self):
-        seq = self.session.merge(fixtures.sequences[0])
-        proj = self.session.merge(fixtures.project)
+        seq = self.session.merge(self.seqs[0])
+        proj = self.session.merge(self.proj)
         
         self.assert_(('Shot', 'sg_sequence') not in seq.backrefs)
         self.assert_(('Shot', 'project') not in proj.backrefs)
@@ -31,9 +42,9 @@ class TestBackrefs(TestCase):
         
     def test_fetch_backref(self):
         
-        proj = self.session.merge(fixtures.project)
-        seqs = [self.session.merge(x) for x in fixtures.sequences]
-        shots = [self.session.merge(x) for x in fixtures.shots]
+        proj = self.session.merge(self.proj)
+        seqs = [self.session.merge(x) for x in self.seqs]
+        shots = [self.session.merge(x) for x in self.shots]
         
         self.assert_(('Shot', 'project') not in proj.backrefs)
         self.assert_(('Sequence', 'project') not in proj.backrefs)
