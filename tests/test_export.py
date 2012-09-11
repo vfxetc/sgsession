822f30a61f902778ec7167b1e43e43bc423415a8
Mike Boers <mboers@keypics.com>
2012-09-11 16:30:10 -0700

    test_export uses sgmock; that is the last one

--- tests/test_export.py
+++ tests/test_export.py
@@ -1,21 +1,29 @@
-from .utils import *
-
-
-def setUpModule():
-    fixtures.setup_sequences()
-    globals().update(fixtures.__dict__)
+from common import *
     
     
 class TestExports(TestCase):
     
+    def setUp(self):
+        sg = Shotgun()
+        self.sg = self.fix = fix = Fixture(sg)
+        self.session = Session(self.sg)
+        
+        proj = fix.Project(mini_uuid())
+        seqs = [proj.Sequence(code, project=proj) for code in ('AA', 'BB')]
+        shots = [seq.Shot('%s_%03d' % (seq['code'], i), project=proj) for seq in seqs for i in range(1, 3)]
+        
+        self.proj = minimal(proj)
+        self.seqs = [minimal(x) for x in seqs]
+        self.shots = [minimal(x) for x in shots]
+    
     def test_minimal(self):
         
-        shot = self.session.merge(fixtures.shots[0])
+        shot = self.session.merge(self.shots[0])
         shot.fetch_heirarchy()
         
         self.assertEqual(shot.minimal, dict(
             type="Shot",
-            id=fixtures.shots[0]['id'],
+            id=self.shots[0]['id'],
         ))
         
         shot.pprint()
