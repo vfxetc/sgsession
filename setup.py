b38eb4f8a7b8e3400ea09c600e241d8c4a9d0846
Mike Boers <mboers@keypics.com>
2012-09-12 11:15:16 -0700

    Add setup so sgfs can install this to test with

---
+++ setup.py
@@ -0,0 +1,24 @@
+from distutils.core import setup
+
+setup(
+    name='sgsession',
+    version='0.1-dev',
+    description='Shotgun ORM/Session.',
+    url='http://github.com/westernx/sgsession',
+    
+    packages=['sgsession'],
+    
+    author='Mike Boers',
+    author_email='sgsession@mikeboers.com',
+    license='BSD-3',
+    
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+    
+)
