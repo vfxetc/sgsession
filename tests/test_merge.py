3b014d22b73f8d03d4803dbb8f8b81c0918ce428
Mike Boers <mboers@keypics.com>
2012-09-12 17:30:11 -0700

    Top level merge override testing

--- tests/test_merge.py
+++ tests/test_merge.py
@@ -5,37 +5,87 @@ class TestMerge(TestCase):
     
     def setUp(self):
         self.session = Session(None)
-
+    
+    def merge(self, **kwargs):
+        return self.session.merge(kwargs)
+    
     def test_setitem(self):
-        a = self.session.merge(dict(a=1))
+        a = self.merge(a=1)
         a['child'] = dict(b=2)
         self.assertEqual(a['child']['b'], 2)
         self.assert_(isinstance(a['child'], Entity))
     
     def test_setdefault(self):
-        a = self.session.merge(dict(a=1))
+        a = self.merge(a=1)
         a.setdefault('child', dict(b=2))
         self.assertEqual(a['child']['b'], 2)
         self.assert_(isinstance(a['child'], Entity))
     
     def test_recursive_update(self):
-        a = self.session.merge(dict(a=1, child=dict(type='Sequence')))
+        a = self.merge(a=1, child=dict(type='Sequence'))
         self.assert_(isinstance(a, Entity))
         self.assert_(isinstance(a['child'], Entity))
         
     def test_simple_update(self):
-        a = self.session.merge(dict(a=1))
-        b = self.session.merge(dict(b=2))
+        a = self.merge(a=1)
+        b = self.merge(b=2)
         a.update(b)
-        self.assertEqual(a, self.session.merge(dict(a=1, b=2)))
+        self.assertEqual(a, self.merge(a=1, b=2))
     
     def test_complex_update(self):
-        a = self.session.merge(dict(sequence=dict(x=0, a=1)))
-        b = self.session.merge(dict(sequence=dict(x=3, b=2)))
+        a = self.merge(sequence=dict(x=0, a=1))
+        b = self.merge(sequence=dict(x=3, b=2))
         a.update(b)
-        self.assertEqual(a, self.session.merge(dict(sequence=dict(a=1, b=2, x=3))))
+        self.assertEqual(a, self.merge(sequence=dict(a=1, b=2, x=3)))
 
     def test_multiple(self):
         a, b = self.session.merge((dict(a=1), dict(b=2)))
-        self.assertEqual(a, self.session.merge(dict(a=1)))
-        self.assertEqual(b, self.session.merge(dict(b=2)))
+        self.assertEqual(a, self.merge(a=1))
+        self.assertEqual(b, self.merge(b=2))
+    
+    def test_over_never(self):
+        
+        a = self.session.merge(dict(type='Dummy', id=1, v='a'))
+        b = self.session.merge(dict(type='Dummy', id=1, v='b'), over=False)
+        self.assertIs(a, b)
+        self.assertEqual(a['v'], 'a')
+        
+        a = self.session.merge(dict(type='Dummy', id=2, v='a', updated_at=1))
+        b = self.session.merge(dict(type='Dummy', id=2, v='b', updated_at=2), over=False)
+        self.assertIs(a, b)
+        self.assertEqual(a['v'], 'a')
+        
+        a = self.session.merge(dict(type='Dummy', id=3, v='a', updated_at=2))
+        b = self.session.merge(dict(type='Dummy', id=3, v='b', updated_at=1), over=False)
+        self.assertIs(a, b)
+        self.assertEqual(a['v'], 'a')
+    
+    def test_over_timed(self):
+        
+        a = self.session.merge(dict(type='Dummy', id=4, v='a', updated_at=1))
+        b = self.session.merge(dict(type='Dummy', id=4, v='b', updated_at=2))
+        self.assertIs(a, b)
+        self.assertEqual(a['v'], 'b')
+        
+        a = self.session.merge(dict(type='Dummy', id=5, v='a', updated_at=2))
+        b = self.session.merge(dict(type='Dummy', id=5, v='b', updated_at=1))
+        self.assertIs(a, b)
+        self.assertEqual(a['v'], 'a')
+    
+    def test_over_always(self):
+    
+        a = self.session.merge(dict(type='Dummy', id=6, v='a'))
+        b = self.session.merge(dict(type='Dummy', id=6, v='b'), over=True)
+        self.assertIs(a, b)
+        self.assertEqual(a['v'], 'b')
+        
+        a = self.session.merge(dict(type='Dummy', id=7, v='a', updated_at=1))
+        b = self.session.merge(dict(type='Dummy', id=7, v='b', updated_at=2), over=True)
+        self.assertIs(a, b)
+        self.assertEqual(a['v'], 'b')
+        
+        a = self.session.merge(dict(type='Dummy', id=8, v='a', updated_at=2))
+        b = self.session.merge(dict(type='Dummy', id=8, v='b', updated_at=1), over=True)
+        self.assertIs(a, b)
+        self.assertEqual(a['v'], 'b')
+        
