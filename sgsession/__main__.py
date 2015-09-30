dfe47ee70757824c65df319800e1eba1c5f6b6d0
Mike Boers <westernx@mikeboers.com>
2015-09-30 13:15:13 -0700

    python -m sgsession Type id -> pprint the entity

--- sgsession/__main__.py
+++ sgsession/__main__.py
@@ -1,5 +1,12 @@
+import sys
+
 from shotgun_api3_registry import connect as _connect
 
 from . import Session
 
 sg = Session(_connect())
+
+
+if len(sys.argv) >= 3:
+    entity = sg.get(sys.argv[1], int(sys.argv[2]), fields=sys.argv[3:])
+    entity.pprint()
